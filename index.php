cf1cdef4ffe838330210fec8147ab0455deb51c6
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2023-09-12 13:41:22 +0200

    main index

--- index.php
+++ index.php
@@ -1,30 +1,73 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Cours PHP / MySQL</title>
+        <title>Edusign - Portail Web</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="cours.css">
+        <link rel="stylesheet" href="assets/style.css">
+        <link rel="shortcut icon" href="assets/img/edusign.png" type="image/x-icon">
     </head>
     <body>
-        <h1>Edusign</h1>  
+
+        <div class="gallery">
+
+            <div class="left" onclick="location.href='school/';">
+                <div class="title-left">
+                    <h1>
+                        Je suis <br> administrateur·rice
+                    </h1>
+                </div>
+            </div>
+
+            <div class="mid">
+                <h1 class="edusign-title">Edusign</h1>
+                <div class="title-mid">
+                    <h1>
+                        Je suis <br> intervenant·e
+                    </h1>
+                </div>
+            </div>
+
+            <div class="right">
+                <div class="title-right">
+                    <h1>
+                        Je suis <br> apprenant·e
+                    </h1>
+                </div>
+            </div>
+
+            <footer>
+                    <a href="https://edusign.fr/">
+                        <span>Site web</span>
+                    </a>
+                    &nbsp; - &nbsp;
+                    <a href="https://edusign.fr/mentions-legales/">
+                        <span>Mentions légales</span>
+                    </a>
+                    &nbsp; - &nbsp;
+                    <a href="https://www.edusign.fr/legal/CGU.pdf">
+                        <span>Conditions générales d'utilisation</span>
+                    </a>
+            </footer>
+
+        </div>
 
 
 
 
 
-        <?php
-            $servername = 'localhost';
-            $username = 'root';
-            $password = 'root';
-            
-            //On établit la connexion
-            $conn = new mysqli($servername, $username, $password);
-            
-            //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
-            }
-            echo 'Connexion réussie';
-        ?>
+            <?php
+                $servername = 'localhost';
+                $username = 'root';
+                $password = 'root';
+                
+                //On établit la connexion
+                $conn = new mysqli($servername, $username, $password);
+                
+                //On vérifie la connexion
+                if($conn->connect_error){
+                    die('Erreur : ' .$conn->connect_error);
+                }
+                echo 'Connexion réussie';
+            ?>
     </body>
 </html>

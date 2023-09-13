f0ee586d6ec5632457cbeee7f079f74fdf125bbd
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2023-09-13 09:45:06 +0200

    apprenant.php

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -2,11 +2,11 @@ var mid = document.querySelector('.mid');
 var right = document.querySelector('.right');
 
 mid.addEventListener('click', function(){
-    var url = "templates/apprenant.php";
+    var url = "templates/intervenant.php";
     window.location.href = url;
 });
 
 right.addEventListener('click', function(){
-    var url = "templates/intervenant.php";
+    var url = "templates/apprenant.php";
     window.location.href = url;
 });
--- templates/apprenant.php
+++ templates/apprenant.php
@@ -51,7 +51,7 @@
 
         </div>
 
-    <form action="POST">
+    <form method="POST" action="formulaire.php">
         <input type="email" name="email" id="email" placeholder=" Email">
         <input type="password" name="password" id="password" placeholder=" Mot de passe">
         <div class="form-btn">

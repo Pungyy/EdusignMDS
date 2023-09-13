cdafb6da3ab9af0d9c6a33d0525df58a13c96285
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2023-09-13 11:14:09 +0200

    apprenant

---
+++ templates/intervenant.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/intervenant.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-inter">
+            <img src="../assets/img/logo.png" alt="">
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="form">
+
+            <div class="intervenant">
+                <h1>CONNEXION <br> <b>INTERVENANT</b></h1>
+            </div>
+
+            <div class="inputs">
+                <input type="email" placeholder="ex. darthvader@gmail.com">
+            </div>
+
+            <div class="inputs2">
+                <input type="password" placeholder="*****">
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>

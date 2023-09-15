1000b1b92e729a1ca1d76013b520a082a1bac05b
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2023-09-15 14:01:54 +0200

    fiche présence

--- templates/fichePresence.php
+++ templates/fichePresence.php
@@ -3,32 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/apprenant.css">
+    <link rel="stylesheet" href="../assets/css/fichePresence.css">
     <?php include_once '../bdd/fichePresenceControleur.php';?>
 
     <title>Présences</title>
 </head>
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th> Nom</th>
-                <th> Prénom </th>
-                <th>Date de Naissance</th>
-                <th>Présence</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($donnees as $ligne) : ?>
-            <tr>
-                <td><?= $ligne['nomEtudiant'] ?></td>
-                <td><?= $ligne['prenomEtudiant'] ?></td>
-                <td><?= $ligne['dateNaissance'] ?></td>
-                <td><?= $ligne['estPresent'] ?></td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+
+    <nav>
+        <div class="nav-inter">
+            <img src="../assets/img/logo.png" alt="Logo Edusign">
+        </div>
+    </nav>
+
+    <div class="tableau">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom </th>
+                    <th>Date de Naissance</th>
+                    <th>Présence</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($donnees as $ligne) : ?>
+                <tr>
+                    <td><?= $ligne['nomEtudiant'] ?></td>
+                    <td><?= $ligne['prenomEtudiant'] ?></td>
+                    <td><?= $ligne['dateNaissance'] ?></td>
+                    <td id="absent"><?= $ligne['estPresent'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
-<script src="../assets/js/apprenant.js"></script>
+
+<script>
+
+    var isAbsent = document.getElementById("absent");
+    const array = [isAbsent];
+
+    array.forEach(element => {
+        if (isAbsent = "ABSENT"){
+        document.getElementById("absent").style.color = "red";
+        }
+        else {
+            document.getElementById("absent").style.color = "green";
+        }
+    });
+       
+</script>  
+
 </html>
